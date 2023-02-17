c8b3c4747516859b0274cbe9bd9cd8260e76c7c1
Brandon <bcarvalho2500@gmail.com>
2023-02-16 22:49:39 -0500

    Complete hw

--- app/src/main/java/com/example/cis433homework2/MainActivity.java
+++ app/src/main/java/com/example/cis433homework2/MainActivity.java
@@ -2,13 +2,76 @@ package com.example.cis433homework2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText addItemField;
+    private ArrayList<String> shoppingList = new ArrayList<>(15);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        ArrayList<String> tempShoppingList = intent.getStringArrayListExtra("ShoppingList");
+        if (tempShoppingList != null){
+            shoppingList = tempShoppingList;
+        }
+
+        addItemField = findViewById(R.id.addItem);
+    }
+
+    private boolean checkListFull(){
+        if (shoppingList.size() == 15){
+            Toast.makeText(this, "Your shopping list is full", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return false;
+    }
+    private boolean checkItemExists(String item){
+        if (shoppingList.contains(item)){
+            Toast.makeText(this, item + " is already in your list", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return false;
+    }
+    public void onAddItemClick(View view) {
+        try{
+            if (checkListFull()) return;
+            if (checkItemExists(addItemField.getText().toString())) return;
+            shoppingList.add(String.valueOf(addItemField.getText()));
+            Toast.makeText(this, addItemField.getText() + " has been successfully added", Toast.LENGTH_SHORT).show();
+            addItemField.setText("");
+            System.out.println(shoppingList);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void onFrequentItemClick(View view){
+        try{
+            Button buttonView = findViewById(view.getId());
+            if (checkListFull()) return;
+            if (checkItemExists(buttonView.getText().toString())) return;
+            shoppingList.add(String.valueOf(buttonView.getText()));
+            Toast.makeText(this, buttonView.getText() + " has been successfully added", Toast.LENGTH_SHORT).show();
+            System.out.println(shoppingList);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void onShowListClick(View view){
+        Intent intent = new Intent(this, ShoppingList.class);
+        intent.putStringArrayListExtra("ShoppingList", shoppingList);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/cis433homework2/ShoppingList.java
+++ app/src/main/java/com/example/cis433homework2/ShoppingList.java
@@ -2,13 +2,75 @@ package com.example.cis433homework2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class ShoppingList extends AppCompatActivity {
 
+    private ArrayList<String> shoppingList = new ArrayList<>(15);
+    private ArrayList<TextView> listView = new ArrayList<TextView>(15);
+    private EditText deleteIndex;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_list);
+
+        listView.add(findViewById(R.id.item_1));
+        listView.add(findViewById(R.id.item_2));
+        listView.add(findViewById(R.id.item_3));
+        listView.add(findViewById(R.id.item_4));
+        listView.add(findViewById(R.id.item_5));
+        listView.add(findViewById(R.id.item_6));
+        listView.add(findViewById(R.id.item_7));
+        listView.add(findViewById(R.id.item_8));
+        listView.add(findViewById(R.id.item_9));
+        listView.add(findViewById(R.id.item_10));
+        listView.add(findViewById(R.id.item_11));
+        listView.add(findViewById(R.id.item_12));
+        listView.add(findViewById(R.id.item_13));
+        listView.add(findViewById(R.id.item_14));
+        listView.add(findViewById(R.id.item_15));
+
+        deleteIndex = findViewById(R.id.indexToDelete);
+
+        Intent intent = getIntent();
+        shoppingList = intent.getStringArrayListExtra("ShoppingList");
+        renderShoppingList();
+    }
+    private void renderShoppingList(){
+        for (int i = 0; i < listView.size(); i++){
+            listView.get(i).setText("");
+        }
+        for (int i = 0; i < shoppingList.size(); i++){
+            listView.get(i).setText(getString(R.string.list_item, i + 1, shoppingList.get(i)));
+        }
+        System.out.println(shoppingList);
+    }
+    public void onDeleteAllClick(View view){
+        shoppingList.clear();
+        renderShoppingList();
+    }
+
+    public void onDeleteSpecificItem(View view){
+        int index = Integer.parseInt(deleteIndex.getText().toString());
+        if (index > shoppingList.size()){
+            Toast.makeText(this, "Not a valid index", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        shoppingList.remove(index - 1);
+        deleteIndex.setText("");
+        renderShoppingList();
+    }
+
+    public void onAddMoreItemsClick(View view) {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        intent.putStringArrayListExtra("ShoppingList", shoppingList);
+        startActivity(intent);
     }
 }
